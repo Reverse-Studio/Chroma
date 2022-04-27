6a81312bbff3933b0992130b7f2fc588bfbe0cca
changwook987 <changwook987@naver.com>
2022-04-27 19:33:51 +0900

    Player

--- Assets/Prefabs/Player/Player.cs
+++ Assets/Prefabs/Player/Player.cs
@@ -1,48 +1,94 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float JumpPower;
     public bool IsSliding;
     public int Health;
-
     private Animator animator;
-    public float FloatingTime
+    private Rigidbody rigid;
+
+    //바닥과의 거리
+    public float PlaneDistance
     {
         get
         {
-            return animator.GetFloat("FloatingTime");
+            return planeDistance;
         }
         set
         {
-            animator.SetFloat("FloatingTime", value);
+            planeDistance = value;
+            animator.SetFloat("PlaneDistance", value);
         }
     }
+
+    public bool IsJumping
+    {
+        get
+        {
+            return animator.GetBool("IsJumping");
+        }
+        set
+        {
+            animator.SetBool("IsJumping", value);
+        }
+    }
+
+    public bool IsRunning
+    {
+        get
+        {
+            return animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "Fast Run";
+        }
+    }
+
+    private float planeDistance;
+    private bool jump;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
+
+
     void Update()
     {
-        timing();
-
-        if (Input.anyKeyDown) doJump();
+        jump = Input.GetKey(KeyCode.UpArrow);
     }
 
-    public void doJump()
+    void FixedUpdate()
     {
-        if (FloatingTime < 0)
+        Distancing();
+        DoJump();
+    }
+
+    private void Distancing()
+    {
+        Debug.DrawRay(transform.position, Vector3.down, Color.green, 1f);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
         {
-            FloatingTime = 2;
+            PlaneDistance = hit.distance;
+        }
+        else
+        {
+            PlaneDistance = 0f;
+            IsJumping = false;
+            rigid.velocity = Vector3.zero;
         }
     }
-    private void timing()
+
+    private void DoJump()
     {
-        if (FloatingTime >= 0)
+        if (IsRunning && jump && IsJumping == false)
         {
-            FloatingTime -= Time.deltaTime;
+            rigid.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+            IsJumping = true;
         }
     }
 }
