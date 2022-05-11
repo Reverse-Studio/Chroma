31de6995c88207a0bb3162297c34a5cfdc6d97db
roopha <80938105+roopgha@users.noreply.github.com>
2022-05-11 19:54:58 +0900

    add note movement

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -4,19 +4,11 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
-    public int notespeed;
-    private GameObject notes;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        notes = GameObject.Find("Note");
-    }
+    public int type;
+    public int speed;
 
-    // Update is called once per frame
     void Update()
-    {       
-         notes.transform.position -= new Vector3(notespeed * Time.deltaTime, 0f, 0f);
+    {
+        transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
     }
-
 }
