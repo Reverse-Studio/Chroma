11389aab9b6331fd0242f64e149e38ab28ab11dc
roopha <80938105+roopgha@users.noreply.github.com>
2022-05-27 17:42:02 +0900

    Fix Code

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -4,12 +4,5 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
-    public float speed = 1f;
-
-    public int type;
-   
-    private void Update()
-    {
-        transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
-    }
+    public int type = 0;
 }
